fc46342ac431c1be1a4b9e0fe5ef86c52addc6ef
kusare <94953298+kusare@users.noreply.github.com>
2022-05-06 11:24:37 +0900

    installed recoil

---
+++ src/components/LengthComponent.js
@@ -0,0 +1,8 @@
+import { useRecoilValue } from "recoil";
+import { lengthState } from "../selectors/name";
+
+export default function LengthComponent() {
+  const length = useRecoilValue(lengthState);
+
+  return <h3 className="length">{length}</h3>;
+}
