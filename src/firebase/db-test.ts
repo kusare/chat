6a8a4e8ac57a332f307bc83254e9b2539fab9089
kusare <94953298+kusare@users.noreply.github.com>
2022-05-10 06:01:45 +0900

    init

---
+++ src/firebase/db-test.ts
@@ -0,0 +1,15 @@
+import { db } from "src/firebase/db";
+import {
+  Firestore,
+  collection,
+  getDocs,
+} from "node_modules/firebase/firestore";
+
+async function getCities(db: Firestore) {
+  const citiesCol = collection(db, "cities");
+  const citySnapshot = await getDocs(citiesCol);
+  const cityList = citySnapshot.docs.map((doc) => doc.data());
+  return cityList;
+}
+
+export default getCities(db);
