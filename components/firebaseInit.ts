1a1fbd64efadbd5d6b86241540badf7645ac8d33
kusare <94953298+kusare@users.noreply.github.com>
2022-05-21 18:33:07 +0900

    ADD todo tree

--- components/firebaseInit.ts
+++ components/firebaseInit.ts
@@ -1,6 +1,5 @@
 import { getApps, getApp, initializeApp, FirebaseApp } from "firebase/app";
 
-// TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
