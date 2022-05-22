f47cfe0876f13ae123d4e5f602cb2be3a8e02184
kusare <94953298+kusare@users.noreply.github.com>
2022-05-22 09:29:12 +0900

    ADD glsl loader

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
-};
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.(glsl|vs|fs|vert|frag)$/,
+      use: ["raw-loader", "glslify-loader"],
+    });
 
-module.exports = nextConfig;
+    return config;
+  },
+};
