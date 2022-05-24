6ea546ea91644825288ef56bf04b95c9d0219884
kusare <94953298+kusare@users.noreply.github.com>
2022-05-24 16:55:07 +0900

    ADD persist atom

--- atoms/cssMsgStates.ts
+++ atoms/cssMsgStates.ts
@@ -1,11 +1,26 @@
 import { atom } from "recoil";
 import { Timestamp } from "firebase/firestore";
 import { MsgState, Msg } from "../types";
+import { recoilPersist } from "recoil-persist";
+
+/**
+██████╗ ███████╗ ██████╗ ██████╗ ██╗██╗         ██████╗ ███████╗██████╗ ███████╗██╗███████╗████████╗
+██╔══██╗██╔════╝██╔════╝██╔═══██╗██║██║         ██╔══██╗██╔════╝██╔══██╗██╔════╝██║██╔════╝╚══██╔══╝
+██████╔╝█████╗  ██║     ██║   ██║██║██║         ██████╔╝█████╗  ██████╔╝███████╗██║███████╗   ██║   
+██╔══██╗██╔══╝  ██║     ██║   ██║██║██║         ██╔═══╝ ██╔══╝  ██╔══██╗╚════██║██║╚════██║   ██║   
+██║  ██║███████╗╚██████╗╚██████╔╝██║███████╗    ██║     ███████╗██║  ██║███████║██║███████║   ██║   
+╚═╝  ╚═╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝╚══════╝    ╚═╝     ╚══════╝╚═╝  ╚═╝╚══════╝╚═╝╚══════╝   ╚═╝   
+                                                                                                    
+ */
+const { persistAtom } = recoilPersist({
+  key: "cssMsgStates", // this key is using to store data in local storage
+});
 
 export const cssTextState = atom({
   key: "cssTextState",
   // default: "color: black; background-color: #8AA058;",
   default: "",
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const cssMsgState = atom<MsgState>({
