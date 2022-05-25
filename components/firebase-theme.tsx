a169d8d293f22211911d114c6e61068c55065ddf
kusare <94953298+kusare@users.noreply.github.com>
2022-05-25 18:32:15 +0900

    ADD card

--- components/firebase-theme.tsx
+++ components/firebase-theme.tsx
@@ -36,6 +36,24 @@ import {
 import { cssMsgsState, cssMsgState, cssTextState } from "../atoms/cssMsgStates";
 import { MsgState, Msg } from "../types";
 
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import IconButton, { IconButtonProps } from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import StarIcon from "@mui/icons-material/Star";
+import FilterVintageIcon from "@mui/icons-material/FilterVintage";
+import Badge from "@mui/material/Badge";
+
 /**
  ██████╗ ███████╗████████╗
 ██╔════╝ ██╔════╝╚══██╔══╝
@@ -134,46 +152,161 @@ export const CssMsg: React.FC<{ msg: MsgState }> = (props) => {
     );
   }, [props.msg?.timestamp]);
 
+  /**
+ * 
+ ██████╗ █████╗ ██████╗ ██████╗ 
+██╔════╝██╔══██╗██╔══██╗██╔══██╗
+██║     ███████║██████╔╝██║  ██║
+██║     ██╔══██║██╔══██╗██║  ██║
+╚██████╗██║  ██║██║  ██║██████╔╝
+ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ 
+                                
+ */
+
+  interface ExpandMoreProps extends IconButtonProps {
+    expand: boolean;
+  }
+
+  const ExpandMore = styled((props: ExpandMoreProps) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+  })(({ theme, expand }) => ({
+    transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+    marginLeft: "auto",
+    transition: theme.transitions.create("transform", {
+      duration: theme.transitions.duration.shortest,
+    }),
+  }));
+
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  //  💅CSS to Return
+  // const cssText = useRecoilValue(cssTextState);
+
+  /**
+██████╗ ███████╗████████╗██╗   ██╗██████╗ ███╗   ██╗
+██╔══██╗██╔════╝╚══██╔══╝██║   ██║██╔══██╗████╗  ██║
+██████╔╝█████╗     ██║   ██║   ██║██████╔╝██╔██╗ ██║
+██╔══██╗██╔══╝     ██║   ██║   ██║██╔══██╗██║╚██╗██║
+██║  ██║███████╗   ██║   ╚██████╔╝██║  ██║██║ ╚████║
+╚═╝  ╚═╝╚══════╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝
+                                                    
+ */
+
   if (!props.msg) return <></>;
 
   return (
     <>
-      <Stack spacing={2} direction="row">
-        <Box>
-          {props.msg.profilePicUrl && (
+      {/* 
+ ██████╗ █████╗ ██████╗ ██████╗ 
+██╔════╝██╔══██╗██╔══██╗██╔══██╗
+██║     ███████║██████╔╝██║  ██║
+██║     ██╔══██║██╔══██╗██║  ██║
+╚██████╗██║  ██║██║  ██║██████╔╝
+ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ 
+                                
+ */}
+
+      <Card
+        sx={{ maxWidth: 345 }}
+        // css={css`
+        //   * {
+        //     ${cssText}
+        //   }
+        // `}
+      >
+        <CardHeader
+          //😭プロフィールの画像
+          action={
             <Avatar
+              sx={{ bgcolor: red[500] }}
+              aria-label="recipe"
               alt="profilePic"
               src={props.msg.profilePicUrl}
-              sx={{ width: 48, height: 48 }}
-            />
-          )}
-        </Box>
-
-        <Box>
-          <Stack spacing={2} direction="row">
-            {props.msg.name && <div>{props.msg.name}</div>}
-            {props.msg.timestamp && <time dateTime={time}>{time}</time>}
-          </Stack>
-          <TextField
-            multiline
-            placeholder="No Comment"
-            maxRows={4}
-            value={props.msg.text}
-          />
-        </Box>
-      </Stack>
-      <div>
+            >
+              <MoreVertIcon />
+            </Avatar>
+          }
+          //📛プロフィールの名前
+          title={props.msg.name}
+          //⌚アップした時間
+          subheader={time}
+        />
+        {/* 🖼アップした画像 */}
         {props.msg.imageUrl && (
-          // eslint-disable-next-line @next/next/no-img-element
-          <img
-            src={props.msg.imageUrl}
-            alt="no image"
-            css={css(`
-          max-width: 100%;
-        `)}
+          <CardMedia
+            component="img"
+            height="50"
+            sx={{ width: 50 }}
+            image={props.msg.imageUrl}
+            alt="Paella dish"
           />
         )}
-      </div>
+
+        {/* 💅CSS Sheet` */}
+        <CardContent>
+          <Typography variant="body2" color="text.secondary">
+            {props.msg.text}
+          </Typography>
+        </CardContent>
+        <CardActions disableSpacing>
+          <IconButton aria-label="add to favorites">
+            <FavoriteIcon />
+          </IconButton>
+          <IconButton aria-label="flower">
+            <FilterVintageIcon />
+          </IconButton>
+          <IconButton aria-label="star">
+            <Badge color="secondary" badgeContent={99}>
+              <StarIcon />
+            </Badge>
+          </IconButton>
+          <ExpandMore
+            expand={expanded}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </ExpandMore>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+          <CardContent>
+            <Typography paragraph>Method:</Typography>
+            <Typography paragraph>
+              Heat 1/2 cup of the broth in a pot until simmering, add saffron
+              and set aside for 10 minutes.
+            </Typography>
+            <Typography paragraph>
+              Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet
+              over medium-high heat. Add chicken, shrimp and chorizo, and cook,
+              stirring occasionally until lightly browned, 6 to 8 minutes.
+              Transfer shrimp to a large plate and set aside, leaving chicken
+              and chorizo in the pan. Add pimentón, bay leaves, garlic,
+              tomatoes, onion, salt and pepper, and cook, stirring often until
+              thickened and fragrant, about 10 minutes. Add saffron broth and
+              remaining 4 1/2 cups chicken broth; bring to a boil.
+            </Typography>
+            <Typography paragraph>
+              Add rice and stir very gently to distribute. Top with artichokes
+              and peppers, and cook without stirring, until most of the liquid
+              is absorbed, 15 to 18 minutes. Reduce heat to medium-low, add
+              reserved shrimp and mussels, tucking them down into the rice, and
+              cook again without stirring, until mussels have opened and rice is
+              just tender, 5 to 7 minutes more. (Discard any mussels that
+              don&apos;t open.)
+            </Typography>
+            <Typography>
+              Set aside off of the heat to let rest for 10 minutes, and then
+              serve.
+            </Typography>
+          </CardContent>
+        </Collapse>
+      </Card>
     </>
   );
 };
