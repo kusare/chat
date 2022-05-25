dd2e73c67fbaa4fad5afeb39bdc5bce46c889bba
kusare <94953298+kusare@users.noreply.github.com>
2022-05-25 21:57:12 +0900

    ADD modal

--- components/firebase-theme.tsx
+++ components/firebase-theme.tsx
@@ -58,6 +58,7 @@ import MenuItem from "@mui/material/MenuItem";
 import EditIcon from "@mui/icons-material/Edit";
 import ElectricBoltIcon from "@mui/icons-material/ElectricBolt";
 import PowerIcon from "@mui/icons-material/Power";
+import Modal from "@mui/material/Modal";
 
 /**
  ██████╗ ███████╗████████╗
@@ -181,6 +182,23 @@ export const CssMsg: React.FC<{ msg: MsgState }> = (props) => {
     setAnchorEl(null);
   };
 
+  // 🖊Edit
+  const style = {
+    position: "absolute" as "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
+
+  const [openEdit, setOpenEdit] = React.useState(false);
+  const handleOpenEdit = () => setOpenEdit(true);
+  const handleCloseEdit = () => setOpenEdit(false);
+
   /**
 ██████╗ ███████╗████████╗██╗   ██╗██████╗ ███╗   ██╗
 ██╔══██╗██╔════╝╚══██╔══╝██║   ██║██╔══██╗████╗  ██║
@@ -259,9 +277,27 @@ export const CssMsg: React.FC<{ msg: MsgState }> = (props) => {
             {time}
           </Typography>
           {/* 🖊 Edit */}
-          <IconButton aria-label="edit">
-            <EditIcon />
-          </IconButton>
+          <div>
+            <IconButton aria-label="edit" onClick={handleOpenEdit}>
+              <EditIcon />
+            </IconButton>
+            <Modal
+              open={openEdit}
+              onClose={handleCloseEdit}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+            >
+              <Box sx={style}>
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                  Text in a modal
+                </Typography>
+                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                  Duis mollis, est non commodo luctus, nisi erat porttitor
+                  ligula.
+                </Typography>
+              </Box>
+            </Modal>
+          </div>
         </CardActions>
       </Card>
     </>
