177ae92f613a58bb5c45292d75c8a7ecccf0e2d4
kusare <94953298+kusare@users.noreply.github.com>
2022-06-06 20:09:32 +0900

    ADD dummy date

---
+++ dummy.ts
@@ -0,0 +1,10 @@
+import { Msg } from "./types";
+
+export const DummyMsg: Msg = {
+  id: "dummy id",
+  date: new Date(),
+  name: "dummy name",
+  text: "dummy text",
+  profilePicUrl: "dummy url",
+  imageUrl: "image url dummy",
+};
