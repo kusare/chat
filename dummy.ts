ba985b543f7b3b66ad717f83f693923bec5bf140
kusare <94953298+kusare@users.noreply.github.com>
2022-06-13 18:09:51 +0900

    Fix chat data

--- dummy.ts
+++ dummy.ts
@@ -5,6 +5,8 @@ export const DummyMsg: Msg = {
   date: new Date(),
   name: "dummy name",
   text: "dummy text",
+  title: "dummy title",
+  chatTxt: "dummy chatTxt",
   profilePicUrl: "dummy url",
   imageUrl: "image url dummy",
 };
