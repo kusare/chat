1ecbb662d9c4f5607531b76a6602d88138b31018
kusare <94953298+kusare@users.noreply.github.com>
2022-06-13 18:15:33 +0900

    Del unwanted

--- types.ts
+++ types.ts
@@ -1,18 +1,4 @@
-import {
-  getFirestore,
-  collection,
-  addDoc,
-  query,
-  orderBy,
-  limit,
-  onSnapshot,
-  getDocs,
-  setDoc,
-  updateDoc,
-  doc,
-  serverTimestamp,
-  Timestamp,
-} from "firebase/firestore";
+import { Timestamp } from "firebase/firestore";
 
 /**
 ███╗   ███╗███████╗ ██████╗ 
