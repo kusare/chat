56e26c285d2db9448377f1ee9ee9344180073b78
kusare <94953298+kusare@users.noreply.github.com>
2022-06-15 09:21:20 +0900

    Add rss from catnose99

--- types.ts
+++ types.ts
@@ -88,3 +88,37 @@ export type ThemeUiTargetId =
  */
 
 export type ChatRadioBtnId = "Normal" | "Recipe";
+
+/**
+██████╗ ██╗   ██╗██╗██╗     ██████╗ ███████╗██████╗ 
+██╔══██╗██║   ██║██║██║     ██╔══██╗██╔════╝██╔══██╗
+██████╔╝██║   ██║██║██║     ██║  ██║█████╗  ██████╔╝
+██╔══██╗██║   ██║██║██║     ██║  ██║██╔══╝  ██╔══██╗
+██████╔╝╚██████╔╝██║███████╗██████╔╝███████╗██║  ██║
+╚═════╝  ╚═════╝ ╚═╝╚══════╝╚═════╝ ╚══════╝╚═╝  ╚═╝
+                                                    
+ */
+
+export type Member = {
+  id: string;
+  name: string;
+  avatarSrc: string;
+  role?: string;
+  bio?: string;
+  sources?: string[];
+  includeUrlRegex?: string;
+  excludeUrlRegex?: string;
+  githubUsername?: string;
+  twitterUsername?: string;
+  websiteUrl?: string;
+};
+
+export type PostItem = {
+  authorId: string;
+  authorName: string;
+  title: string;
+  link: string;
+  contentSnippet?: string;
+  isoDate?: string;
+  dateMiliSeconds: number;
+};
