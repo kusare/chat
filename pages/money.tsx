54dc23e6c9776c949acc753d1232303da400d811
kusare <94953298+kusare@users.noreply.github.com>
2022-06-19 10:10:42 +0900

    Fix build error

--- pages/money.tsx
+++ pages/money.tsx
@@ -1,21 +1,5 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { Box, Stack, Button, TextField } from "@mui/material";
-import {
-  signIn,
-  signOutUser,
-  ProfilePic,
-  UserName,
-  setChatMsg,
-  ChatMsgEle,
-  useGetMsgs,
-  useGetChatSubMsgs,
-  setImgMsg,
-  ChatMsgRecipiLayout,
-  SubChatMsgRecipiLayout,
-} from "../components/ChatFirebase";
-import Grid from "@mui/material/Grid";
-import { Input } from "@mui/material";
 import React, { useState, useEffect, useRef } from "react";
 import { Global, css } from "@emotion/react";
 import { atom, useRecoilValue, useSetRecoilState } from "recoil";
