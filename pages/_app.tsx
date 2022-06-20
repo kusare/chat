a73c4746664918e5047e0939333e5cdac73c5f0b
kusare <94953298+kusare@users.noreply.github.com>
2022-06-20 09:40:18 +0900

    Add reset css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import { Provider } from "react-redux";
 import { persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "../redux/store";
+import "modern-css-reset/dist/reset.min.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
