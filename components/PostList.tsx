0f2b041a32909332b88024ae7d560d81574c9f8a
kusare <94953298+kusare@users.noreply.github.com>
2022-06-21 14:19:39 +0900

    Add img to reader

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -10,6 +10,8 @@ import {
   getMemberById,
 } from "../utils/helper";
 import { Stack } from "@mui/material";
+// @ts-ignore
+import DomParser from "dom-parser";
 
 dayjs.extend(relativeTime);
 
@@ -99,35 +101,40 @@ const CustomPostLink: React.FC<{ item: PostItem }> = (props) => {
 
   const { hostname, origin } = new URL(link);
 
+  const rawParser = new DomParser();
+  const doms = rawParser.parseFromString(encoded);
+  const test = doms
+    .getElementsByTagName("img")[0]
+    ?.attributes.filter((dom: { name: string }) => dom.name === "src");
+  let imgUrl = "";
+  if (test) imgUrl = test[0].value;
+
+  const parser = new DomParser();
+
   return (
     <article className="post-link">
       <a href={link} className="post-link__main-link">
+        <img src={imgUrl} />
         <Stack spacing={1} direction="row">
           <h3 className="post-link__title">{title}</h3>
         </Stack>
       </a>
-      <Link href={getMemberPath(member.id)} passHref>
-        <Stack spacing={1} direction="row">
-          {hostname && (
-            <>
-              <img
-                src={getFaviconSrcFromOrigin(origin)}
-                width={14}
-                height={14}
-                className="post-link__site-favicon"
-                alt={hostname}
-              />
-              {hostname}
-            </>
-          )}
-          <div>{member.name}</div>
-          <time dateTime={isoDate}>{dayjs(isoDate).fromNow()}</time>
-        </Stack>
-      </Link>
-      <p>encoded:{encoded}</p>
-      {/* {dateMiliSeconds && dateMiliSeconds > Date.now() - 86400000 * 3 && (
-        <div className="post-link__new-label">NEW</div>
-      )} */}
+      <Stack spacing={1} direction="row">
+        {hostname && (
+          <>
+            <img
+              src={getFaviconSrcFromOrigin(origin)}
+              width={14}
+              height={14}
+              className="post-link__site-favicon"
+              alt={hostname}
+            />
+            {hostname}
+          </>
+        )}
+        <div>{member.name}</div>
+        <time dateTime={isoDate}>{dayjs(isoDate).fromNow()}</time>
+      </Stack>
     </article>
   );
 };
